c97c155b46f49995510f04b42b59c88b11787b1d
Walter Carvalho <waltervascarvalho@gmail.com>
2013-04-20 17:17:11 +0100

    dashboard view

--- DDCH/core/views.py
+++ DDCH/core/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 from django.contrib.auth.views import login
-from django.shortcuts import redirect
-
+from django.shortcuts import redirect, render
 
 def custom_login(request, **kwargs):
     if request.user.is_authenticated():
         return redirect('/', **kwargs)
     else:
         return login(request, template_name='core/login.html')
+
+def dashboard(request):
+  	return render(request, 'core/dashboard.html')
