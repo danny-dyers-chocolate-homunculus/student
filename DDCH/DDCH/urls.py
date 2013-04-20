466b89b8b8c8f54febd44a70038e94b9e530a642
Walter Carvalho <waltervascarvalho@gmail.com>
2013-04-20 15:37:29 +0100

    starting my branch

--- DDCH/DDCH/urls.py
+++ DDCH/DDCH/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'DDCH.views.home', name='home'),
+    url(r'^$', 'core.views.login', name='home'),
     # url(r'^DDCH/', include('DDCH.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- DDCH/core/views.py
+++ DDCH/core/views.py
@@ -1,1 +1,5 @@
 # Create your views here.
+from django.shortcuts import render
+
+def login(request):
+	return render(request, "core/login.html")
