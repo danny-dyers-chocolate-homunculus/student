80476b2475327e01efcbd2b1d8e92641e527d1ec
Joseph Redfern <joseph@redfern.me>
2013-04-20 10:49:49 +0100

    Added

--- DDCH/bills/models.py
+++ DDCH/bills/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core import AbstractBase
+from django.conf import settings
 
 # Create your models here.
+
+
+class FixedBill(AbstractBase):
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(min_value=0, max_digits=8, decimal_places=2)
+    user = models.ForeignKey(user.AUTH_USER_MODEL)
