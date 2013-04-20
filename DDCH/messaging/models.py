798de12fa533d5118b40f63e6e3b78f77eb53386
Joseph Redfern <joseph@redfern.me>
2013-04-20 17:48:06 +0100

    added twilio stuff

--- DDCH/messaging/models.py
+++ DDCH/messaging/models.py
@@ -3,6 +3,7 @@ from core.models import AbstractBase
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.conf import settings
+from twilio.rest import TwilioRestClient
 
 
 class Message(AbstractBase):
@@ -16,4 +17,10 @@ class Message(AbstractBase):
     )
     sent_date = models.DateField(auto_now_add=True)
     message = models.TextField(max_length=65535)
-    read_date = models.DateField(null=True, blank=True)
+    read_date = models.DateField()
+
+    def save(self):
+        account = "AC54e70c20c39d9c982827a1a7f5c926f4"
+        token = "2732c9ae78b018ccaed3950da9531fa3"
+        client = TwilioRestClient(account, token)
+        super(Message, self).save()
