7443359296f09d43324e12f0bcafde482791ed9f
Joseph Redfern <joseph@redfern.me>
2013-04-21 15:32:58 +0100

    updated dealings - makes posts now\!

--- DDCH/issues/models.py
+++ DDCH/issues/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from core.models import AbstractBase, House
+from core.models import AbstractBase, House, Post
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.conf import settings
@@ -9,12 +9,42 @@ from twilio.rest import TwilioRestClient
 class Issue(AbstractBase):
     title = models.CharField(max_length=255)
     house = models.ForeignKey(House)
-    room = models.ForeignKey(Room, null=True, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     description = models.TextField(max_length=65535)
     creation_date = models.DateField(auto_now_add=True)
     resolved = models.BooleanField(default=False)
     resolution_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            super(Issue, self).save()
+            if not self.author.landlord and self.author.phone:
+                client = TwilioRestClient(settings.TWILLIO_ACCOUNT, settings.TWILLIO_TOKEN)
+                message = client.sms.messages.create(
+                    to=self.house.landlord.phone, from_="+441827231000", body="Issue Raised: "+self.title+" on "+self.house.name)
+
+            post = Post()
+            post.text = "The issue '"+self.title+"' has been raised"
+            post.posted_by = self.author
+            post.save()
+
+        if self.resolved:
+            super(Issue, self).save()
+            for user in self.house.user_set.all():
+                if user.phone:
+                    client = TwilioRestClient(settings.TWILLIO_ACCOUNT, settings.TWILLIO_TOKEN)
+                    message = client.sms.messages.create(
+                        to=user.phone, from_="+441827231000", body="The issue '"+self.title+"' has been resolved.")
+
+            post = Post()
+            post.text = "The issue '"+self.title+" has been resolved"
+            post.posted_by = self.house.landlord
+            post.save()
+        else:
+            super(Issue, self).save()
+
+    def __unicode__(self):
+        return self.title
 
 
 class IssueComment(AbstractBase):
