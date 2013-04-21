0f74f4a25ae6ac844411f7c297f596f24ab658ba
Charlie Mills <millscr1@cf.ac.uk>
2013-04-21 12:09:27 +0100

    Added custom logout with messages

--- DDCH/DDCH/urls.py
+++ DDCH/DDCH/urls.py
@@ -8,8 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^login/$', 'core.views.custom_login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-                          {'next_page': '/'}),
+    url(r'^logout/$', 'core.views.custom_logout', name='logout'),
 
 
     url(r'^$', DashboardView.as_view(), name='home'),
