7822fb85e5ca145c3d1d96b2e471b4b48519a715
Joseph Redfern <joseph@redfern.me>
2013-04-21 11:01:29 +0100

    re-update URLS

--- DDCH/DDCH/urls.py
+++ DDCH/DDCH/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from core.views import DashboardView
 from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
